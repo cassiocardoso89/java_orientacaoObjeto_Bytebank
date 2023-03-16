37a760f419ce91d623e07aadc8c163a893af431f
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-03-16 11:03:42 -0300

    Herança e polimorfismo

---
+++ src/TesteContas.java
@@ -0,0 +1,14 @@
+public class TesteContas {
+    public static void main(String[] args) {
+        ContaCorrente cc1 = new ContaCorrente(111,111);
+        cc1.deposita(100);
+
+        ContaPoupanca cp1 = new ContaPoupanca(222, 222);
+        cp1.deposita(200);
+
+        cc1.transfere(10, cp1);
+
+        System.out.println("CC: "+ cc1.getSaldo());
+        System.out.println("CP: "+ cp1.getSaldo());
+    }
+}
